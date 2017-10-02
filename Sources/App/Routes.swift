d25deec67ae71e1b6b6a5578bfbf107afaeb0d16
Attila Nemet <me@attilanemet.com>
2017-10-02 02:13:35 +0100

    Initialized project

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -2,24 +2,8 @@ import Vapor
 
 extension Droplet {
     func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
+        get("/") { request in
+            return try self.view.make("index.html")
         }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
-        try resource("posts", PostController.self)
     }
 }
