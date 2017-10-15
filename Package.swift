2072504c695867b665f8ecdd401cc14042cdf41f
Attila Nemet <me@attilanemet.com>
2017-10-15 14:10:30 +0100

    Added PostgreSQL driver

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor-community/postgresql-provider.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/Zewo/zlib.git", majorVersion: 0, minor: 4)
     ],
     exclude: [
