bfa9136cdf8ff42aa56788884bdce9062768ce6d
Attila Nemet <me@attilanemet.com>
2018-01-27 17:43:07 +0000

    Fix

--- Sources/App/Business/SpendingBusinessLogic.swift
+++ Sources/App/Business/SpendingBusinessLogic.swift
@@ -56,10 +56,8 @@ final class SpendingBusinessLogic {
                                  TransactionSource.externelRegularInbound.rawValue)
                 try group.filter(Transaction.Constants.createdKey, .greaterThanOrEquals, now.startOfWeek)
                 try group.filter(Transaction.Constants.createdKey, .lessThanOrEquals, now)
-                try group.or { orGroup in
-                    try group.filter(Transaction.Constants.amountKey, .lessThanOrEquals, -user.largeTransaction)
-                    try group.filter(Transaction.Constants.amountKey, .greaterThanOrEquals, user.largeTransaction)
-                }
+                try group.filter(Transaction.Constants.amountKey, .greaterThan, -user.largeTransaction)
+                try group.filter(Transaction.Constants.amountKey, .lessThan, user.largeTransaction)
             }
             .all()
 
@@ -116,10 +114,8 @@ final class SpendingBusinessLogic {
                 try group.filter(Transaction.Constants.sourceKey,
                                  .notEquals,
                                  TransactionSource.externelRegularInbound.rawValue)
-                try group.or { orGroup in
-                    try group.filter(Transaction.Constants.amountKey, .lessThanOrEquals, -user.largeTransaction)
-                    try group.filter(Transaction.Constants.amountKey, .greaterThanOrEquals, user.largeTransaction)
-                }
+                try group.filter(Transaction.Constants.amountKey, .greaterThan, -user.largeTransaction)
+                try group.filter(Transaction.Constants.amountKey, .lessThan, user.largeTransaction)
                 try group.filter(Transaction.Constants.createdKey, .greaterThanOrEquals, payday)
                 try group.filter(Transaction.Constants.createdKey, .lessThan, startOfWeek)
             }
