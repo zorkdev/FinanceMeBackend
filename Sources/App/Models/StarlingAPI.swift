3e40f84d3c831177eb60a7b318bdd255821e31d7
Attila Nemet <me@attilanemet.com>
2018-10-20 17:59:07 +0100

    Remove unused API

--- Sources/App/Models/StarlingAPI.swift
+++ Sources/App/Models/StarlingAPI.swift
@@ -1,7 +1,6 @@
 enum StarlingAPI: String {
     static let baseURL = "https://api.starlingbank.com/api/v1/"
 
-    case getBalance = "accounts/balance"
     case getTransactions = "transactions"
 
     var uri: String {
