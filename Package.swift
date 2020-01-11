8a20268512406b46f0e905d08e801d263ad54b07
Attila Nemet <me@attilanemet.com>
2020-01-11 15:07:02 +0000

    Remove public website

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Zorkdev",
+    name: "FinanceMeBackend",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "3.3.0")),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", .upToNextMajor(from: "1.0.0")),
