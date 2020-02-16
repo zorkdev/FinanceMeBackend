eca44bcdd6a4a639e533485f13fe08a0dc7b9364
Attila Nemet <me@attilanemet.com>
2020-02-16 11:39:25 +0000

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "FinanceMeBackend",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.2"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.3"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.4"),
         .package(url: "https://github.com/realm/SwiftLint.git", from: "0.39.1")
